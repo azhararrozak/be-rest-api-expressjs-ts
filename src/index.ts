3aca89f765dbe2e85693d8ebb5426f1e95d231ad
Azhar Arrozak <azhardrozak2001@upi.edu>
2025-03-21 20:42:58 +0700

    feat: add library Express TS

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log("Hello, Rest API Express Typescript")
+import express, { Application, Request, Response, NextFunction } from "express";
+
+const app: Application = express();
+const port: Number = 4000;
+
+app.use("/", (req: Request, res: Response, next: NextFunction) => {
+    res.status(200).send({
+        data: "Hello Express TS"
+    })
+})
+
+app.listen(port, () => {
+    console.log(`Server Listening in port ${port}`)
+});
