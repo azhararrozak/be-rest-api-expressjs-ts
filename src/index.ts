5d72653fe322fe5c74f700ed24f8aa3c61f76077
Azhar Arrozak <azhardrozak2001@upi.edu>
2025-03-22 15:50:21 +0700

    feat: add prettier and eslint configuration in project

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,15 @@
-import express, { Application, Request, Response, NextFunction } from "express";
+import express, { Application, Request, Response, NextFunction } from 'express';
 
 const app: Application = express();
 const port: Number = 4000;
 
-app.use("/", (req: Request, res: Response, next: NextFunction) => {
-    res.status(200).send({
-        data: "Hello Express TS"
-    })
-})
+app.use('/', (req: Request, res: Response, next: NextFunction) => {
+  res.status(200).send({
+    status: '200',
+    data: 'Hello Express TS'
+  });
+});
 
 app.listen(port, () => {
-    console.log(`Server Listening in port ${port}`)
+  console.log(`Server Listening in port ${port}`);
 });
